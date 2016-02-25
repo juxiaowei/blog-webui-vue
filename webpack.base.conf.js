a5418208f479877bdc4fb1b35b4dd71c0ecaede4
hades <Slimeria37@gmail.com>
2016-02-25 18:37:04 +0800

    添加cdn域名

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'build'),
-    publicPath: '/build/',
+    publicPath: 'http://iamhades.oss-cn-shanghai.aliyuncs.com/',
     filename: '[name].js'
   },
   resolve: {
@@ -49,7 +49,7 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'url',
         query: {
-          limit: 100000000000000,
+          limit: 10000,
           name: '[name].[ext]?[hash:7]'
         }
       }
